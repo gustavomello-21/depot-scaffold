7a61eabde85d7248619c181a9dce7c175421f7c4
Gustavo Melo <gmelo6546@gmail.com>
2021-03-06 02:34:20 -0300

    adicionando validates e mudando as mensagens

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greather_than_or_equal: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: "aceita apenas formatos, GIF, JPG e PNG"
+  }
+
 end
