4647ad41bf3d02440b333727df860a6d06be277d
Gustavo Melo <gmelo6546@gmail.com>
2021-04-15 23:40:50 -0300

    adicionando o teste do controller store

---
+++ spec/controllers/store_controller_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe StoreController, type: :controller do
+	describe "Get index" do
+	  it 'returns 200' do
+	  	get :index
+	  	
+	  	expect(response).to have_http_status(:ok)
+	  end
+	end
+  
+end
